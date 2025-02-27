59cd8bce3eac6fae737ec1f13c52404eec025ee7
kein <kein10119810@gmail.com>
2025-02-27 22:08:54 +0900

    jirojava

--- Person/src/Tset.java
+++ Person/src/Tset.java
@@ -8,6 +8,15 @@ public class Tset {
 
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+
+		Person jiro=new Person();
+		jiro.name="木村次郎";
+		jiro.age=18;
+
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+
+
 	}
 
 }
