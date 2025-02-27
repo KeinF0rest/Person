ef891f0dd60d3d6d8ab30b6b9fe4aa1c03a1f5a9
kein <kein10119810@gmail.com>
2025-02-27 22:28:52 +0900

    phone

--- Person/src/Tset.java
+++ Person/src/Tset.java
@@ -5,30 +5,46 @@ public class Tset {
 		Person taro=new Person();
 		taro.name="山田太郎";
 		taro.age=20;
+		taro.phoneNumber="090-0000-0000";
+		taro.address="taro@gmail.com";
 
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+		System.out.println(taro.phoneNumber);
+		System.out.println(taro.address);
 
 		Person jiro=new Person();
 		jiro.name="木村次郎";
 		jiro.age=18;
+		jiro.phoneNumber="090-0000-0001";
+		jiro.address="jiro@gmail.com";
 
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+		System.out.println(jiro.phoneNumber);
+		System.out.println(jiro.address);
 
 		Person hanako=new Person();
 		hanako.name="鈴木花子";
 		hanako.age=16;
+		hanako.phoneNumber="090-0000-0002";
+		hanako.address="hanako@gmail.com";
 
 		System.out.println(hanako.name);
 		System.out.println(hanako.age);
+		System.out.println(hanako.phoneNumber);
+		System.out.println(hanako.address);
 
 		Person kein=new Person();
 		kein.name="工藤慶胤";
 		kein.age=23;
+		kein.phoneNumber="090-0000-0003";
+		kein.address="kein@gmail.com";
 
 		System.out.println(kein.name);
 		System.out.println(kein.age);
+		System.out.println(kein.phoneNumber);
+		System.out.println(kein.address);
 	}
 
 }
